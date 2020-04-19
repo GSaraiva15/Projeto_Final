c9c133398f8ad634a179de8430dc21789021fc03
Gonçalo Saraiva <goncalosaraiva00@gmail.com>
2020-04-19 17:56:12 +0100

    começo da criação de intents (não acabados)

--- app/src/main/java/com/example/projeto_final/MainActivity.java
+++ app/src/main/java/com/example/projeto_final/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.projeto_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void inserirDoente (View view){
+
+        Intent intent = new Intent(this,DisplayInserirDoentes.class);
+    }
+    public void verEstatisticas (View view){
+
+        Intent intent2 = new Intent(this,DisplayVerEstatisticas.class);
     }
 }
