53f7ce244018080402e8790be3780cda9387a0fb
Gonçalo Saraiva <goncalosaraiva00@gmail.com>
2020-04-24 19:15:16 +0100

    Adicionado spinner para o sexo dos doentes

--- app/src/main/java/com/example/projeto_final/mapa.java
+++ app/src/main/java/com/example/projeto_final/mapa.java
@@ -2,15 +2,13 @@ package com.example.projeto_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class DisplayVerEstatisticas extends AppCompatActivity {
+public class mapa extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_ver_estatisticas);
-        Intent intentEstatistica = getIntent();
+        setContentView(R.layout.activity_mapa);
     }
 }
